a924bf08cb8d9016257c0f2ba1e8dba9ae8fc5b6
samirpatel017 <53654947+samirpatel017@users.noreply.github.com>
2020-08-17 15:25:17 +0530

    commit 4
    search bar added

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,7 +6,25 @@ $('.home').html('<i class="fas fa-home"></i> <b>HOME</b>').css('background-color
 $('.office').html('<i class="fas fa-briefcase"></i> <b>OFFICE</b>').css('background-color','#e3ffa7');
 $("#allcheck").click(function(){
     $("input[type=checkbox]").prop('checked', $(this).prop('checked'));
-Array.from($("input[type=checkbox]")).forEach((e)=>{
-console.log($("input[type=checkbox]").val())
 });
+
+let searchTxt = document.getElementById('searchTxt');
+searchTxt.addEventListener('input',function(){
+    let searchIpt = searchTxt.value.toUpperCase();
+    // console.log("text",searchIpt);
+    
+    let noteCard=document.getElementsByClassName('tr_animate');
+    Array.from(noteCard).forEach(e => {
+        let cardTxt = e.getElementsByClassName("cati")[0].innerText;
+        if (cardTxt.includes(searchIpt)) {
+            // console.log("match find",cardTxt);
+            e.style.display = "table-row";
+            // e.style.flexDirection = "flex-row";
+            
+            
+        } else {
+            e.style.display = "none";
+            
+        }
+    })
 });
